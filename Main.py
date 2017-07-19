757ee5b7ce075b40097b2c046e6a315c1efe74c9
Techiephile <spencer@techiephile.tech>
2017-07-18 22:03:31 -0700

    Cleaned up the code
    Cleaned up the code, fixed a few minor issues

--- Main.py
+++ Main.py
@@ -1,41 +1,63 @@
-#imports
+# Imports
 import webbrowser
 import time
 
-#intro
+# intro
 
 print("")
 print("")
-print("$$$$$$$\   $$$$$$\  $$\      $$\ $$$$$$$\     $$$$$$\   $$$$$$\  $$\      $$\ $$$$$$$$\      $$$$$$$\  $$\   $$\ $$\     $$\ $$$$$$$$\ $$$$$$$\ ")
-print("$$  __$$\ $$  __$$\ $$$\    $$$ |$$  __$$\    $$  __$$\ $$  __$$\ $$$\    $$$ |$$  _____|    $$  __$$\ $$ |  $$ |\$$\   $$  |$$  _____|$$  __$$\ ")
-print("$$ |  $$ |$$ /  \__|$$$$\  $$$$ |$$ |  $$ |   $$ /  \__|$$ /  $$ |$$$$\  $$$$ |$$ |          $$ |  $$ |$$ |  $$ | \$$\ $$  / $$ |      $$ |  $$ |")
-print("$$$$$$$  |$$ |      $$\$$\$$ $$ |$$$$$$$  |   $$ |$$$$\ $$$$$$$$ |$$\$$\$$ $$ |$$$$$\        $$$$$$$\ |$$ |  $$ |  \$$$$  /  $$$$$\    $$$$$$$  |")
-print("$$  ____/ $$ |      $$ \$$$  $$ |$$  __$$<    $$ |\_$$ |$$  __$$ |$$ \$$$  $$ |$$  __|       $$  __$$\ $$ |  $$ |   \$$  /   $$  __|   $$  __$$<")
-print("$$ |      $$ |  $$\ $$ |\$  /$$ |$$ |  $$ |   $$ |  $$ |$$ |  $$ |$$ |\$  /$$ |$$ |          $$ |  $$ |$$ |  $$ |    $$ |    $$ |      $$ |  $$ |")
-print("$$ |      \$$$$$$  |$$ | \_/ $$ |$$ |  $$ |   \$$$$$$  |$$ |  $$ |$$ | \_/ $$ |$$$$$$$$\     $$$$$$$  |\$$$$$$  |    $$ |    $$$$$$$$\ $$ |  $$ |")
-print("\__|       \______/ \__|     \__|\__|  \__|    \______/ \__|  \__|\__|     \__|\________|    \_______/  \______/     \__|    \________|\__|  \__|")
+print("""
+
+$$$$$$$\   $$$$$$\  $$\      $$\ $$$$$$$\     $$$$$$\   $$$$$$\  $$\      $$\ $$$$$$$$\\
+$$  __$$\ $$  __$$\ $$$\    $$$ |$$  __$$\    $$  __$$\ $$  __$$\ $$$\    $$$ |$$  _____|
+$$ |  $$ |$$ /  \__|$$$$\  $$$$ |$$ |  $$ |   $$ /  \__|$$ /  $$ |$$$$\  $$$$ |$$ |
+$$$$$$$  |$$ |      $$\$$\$$ $$ |$$$$$$$  |   $$ |$$$$\ $$$$$$$$ |$$\$$\$$ $$ |$$$$$\\
+$$  ____/ $$ |      $$ \$$$  $$ |$$  __$$<    $$ |\_$$ |$$  __$$ |$$ \$$$  $$ |$$  __|
+$$ |      $$ |  $$\ $$ |\$  /$$ |$$ |  $$ |   $$ |  $$ |$$ |  $$ |$$ |\$  /$$ |$$ |
+$$ |      \$$$$$$  |$$ | \_/ $$ |$$ |  $$ |   \$$$$$$  |$$ |  $$ |$$ | \_/ $$ |$$$$$$$$\\
+\__|       \______/ \__|     \__|\__|  \__|    \______/ \__|  \__|\__|     \__|\________|
+
+                   $$$$$$$\  $$\   $$\ $$\     $$\ $$$$$$$$\ $$$$$$$\\
+                   $$  __$$\ $$  __$$\ $$$\   $$  |$$  _____|$$  __$$\\
+                   $$ |  $$ |$$ |  $$ | \$$\ $$  / $$ |      $$ |  $$ |
+                   $$$$$$$\ |$$ |  $$ |  \$$$$  /  $$$$$\    $$$$$$$  |
+                   $$  __$$\ $$ |  $$ |   \$$  /   $$  __|   $$  __$$<
+                   $$ |  $$ |$$ |  $$ |    $$ |    $$ |      $$ |  $$ |
+                   $$$$$$$  |\$$$$$$  |    $$ |    $$$$$$$$\ $$ |  $$ |
+                   \______/     \__|    \________|\__|  \__| \__|  \__|
+
+        """)
 print("Version 0.01")
 print("")
 print("It's nice to have choices!")
 print("")
+print("For questions, comments, suggestions please visit - ")
 print("http://techiephile.tech")
 print("")
-print("This program will enter your search term on 6 major online game stores and open them in your browser.")
+print("This program will enter your search term on 6 major online game stores and open them in your browser. ")
 print("")
+
+
+# Search again function
 
 
 def redo():
-    goagain = input("Do you want to keep searching? [y]/[n]: ")
+    goagain = input("Do you want to run another search? [y]/[n]: ")
     if goagain == "y":
+        main()
     elif goagain == "n":
         print("K bye then")
         exit()
     else:
         print("Please choose y or n")
+        redo()
+
+# Main function
 
 
 def main():
-    game = input("What game do you want to purchase? ")
+    game = input("Enter a game title to search for? ")
+    print("Scouring the internet...")
     greenmangaming = "https://www.greenmangaming.com/search/" + game
     steam = "http://store.steampowered.com/search/?term=" + game
     g2a = "https://www.g2a.com/?search=" + game
@@ -43,6 +65,7 @@ def main():
     origin = "https://www.origin.com/usa/en-us/search?searchString=" + game
     kinguin = "https://www.kinguin.net/catalogsearch/result/index/?q=" + game
     webbrowser.open_new(greenmangaming)
+    print("You should see a browser window open now!")
     time.sleep(5)
     webbrowser.open(steam)
     webbrowser.open(g2a)
